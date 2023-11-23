3a7d73ef60a861202ed22b61b2b149a20fab9eb3
Jonathan Hartman <j@hartman.io>
2023-11-23 00:37:12 -0800

    Add IPv6 support
    If the IP address passed in is recognizably a v6, create a AAAA record.

--- tf/main.tf
+++ tf/main.tf
@@ -5,7 +5,7 @@ terraform {
 resource "porkbun_dns_record" "nasty_root" {
   domain  = var.domain
   name    = ""
-  type    = "A"
+  type    = can(cidrhost("${var.ip_address}/128", 0)) ? "AAAA" : "A"
   content = var.ip_address
   ttl     = 600
   notes   = "Root domain A record"
@@ -14,7 +14,7 @@ resource "porkbun_dns_record" "nasty_root" {
 resource "porkbun_dns_record" "nasty_wildcard" {
   domain  = var.domain
   name    = "*"
-  type    = "A"
+  type    = can(cidrhost("${var.ip_address}/128", 0)) ? "AAAA" : "A"
   content = var.ip_address
   ttl     = 600
   notes   = "Wildcard DNS for all NAS services"
