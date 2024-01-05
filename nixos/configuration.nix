cd5542a0adf1bc638fd1f0c2656e34bcd9e16981
Jonathan Hartman <j@hartman.io>
2024-01-04 21:48:32 -0800

    Set up my first SSH user

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,9 +1,26 @@
 {config, pkgs, ...}:
 
 {
+  users.mutableUsers = false;
+
+  users.users.cheese = {
+    isNormalUser = true;
+    description = "It's me";
+    extraGroups = ["wheel"];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICWJKtUplJ5KOY7a0Zrl/EswQDLwCpVTt7zI+5zlu9jp nasty"
+    ];
+  }
+
   environment.systemPackages = [
     pkgs.jellyfin
     pkgs.jellyfin-web
     pkgs.jellyfin-ffmpeg
   ];
+
+  services.openssh = {
+    enable = true;
+    settings.PasswordAuthentication = false;
+    settings.KbdInteractiveAuthentication = false;
+  }
 }
