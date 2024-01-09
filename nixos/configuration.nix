976c7a3cc09e2d135db996c0a959efaf79ef68b0
Jonathan Hartman <j@hartman.io>
2024-01-08 23:55:15 -0800

    Add a static IP address (I think)

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -4,6 +4,13 @@
   networking.hostName = "missjackson";
   networking.domain = "nasty.monster"
 
+  networking.interfaces.eth0.ipv4.addresses = [
+    {
+      address = "192.168.0.69";
+      prefixLength = 24;
+    }
+  ];
+
   users.mutableUsers = false;
 
   users.users.cheese = {
