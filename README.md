ec002c2524b0e30b7b208b98e92f1ec39cc8c66f
Jonathan Hartman <j@hartman.io>
2024-01-12 23:58:36 -0800

    Make a first attempt at a self-installing ISO
    Doesn't actually install NixOS and do the final reboot yet. Just wipes and
    repartitions the root drive. The server isn't yet up and running so this is
    safe for me to play with while I test it out.

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# nasty
+# NASty
+
+All the non-secret things I use to run my NAS.
+
+This is a work in progress. I'm a NixOS noob. Do not trust anything in this
+repo.
+
+*There is destructive code in the `nixos` directory. Do not use it or the ISO
+artifacts it produces unless you want to wipe your root drive.
+
+## Contents
+
+### /nixos
+
+Code to build a custom NixOS installer ISO that, when used to boot a system,
+*will destroy all root drive data* and reprovision it with a fresh NixOS
+install plus any NAS-specific goodies I choose to add.
+
+### /tf
+
+OpenTofu/Terraform components; right now just DNS records.
--- nixos/configuration-installer.nix
+++ nixos/configuration-installer.nix
@@ -4,4 +4,42 @@
   imports = [
     ./configuration-shared.nix
   ];
+
+  # Automate the installation via a run-once systemd service on the installer
+  # image. Adapted from
+  # https://github.com/tfc/nixos-offline-installer/blob/master/installer-configuration.nix
+  let
+    rootDevice = builtins.getEnv "NIXOS_ROOT_DEVICE";
+  in {
+    systemd.services.nixos-installer = {
+      description = "NixOS installer";
+      wantedBy = [ "multi-user.target" ];
+      after = [ "getty.target" "ncsd.service" ];
+      serviceConfig = {
+        Type = "oneshot";
+        RemainAfterExit = "yes";
+      };
+
+      script = ''
+        set -euxo pipefail
+
+        wipefs -fa ${rootDevice}}
+
+        parted /dev/${rootDevice} -- mklabel gpt
+        parted /dev/${rootDevice} -- mkpart root ext4 512MB -8GB
+        parted /dev/${rootDevice} -- mkpart swap linux-swap -8GB 100%
+        parted /dev/${rootDevice} -- mkpart ESP fat32 1MB 512MB
+        parted /dev/${rootDevice} -- set 3 esp on
+
+        mkfs.ext4 -L nixos /dev/${rootDevice}1
+        mkswap -L swap /dev/${rootDevice}2
+        swapon /dev/${rootDevice}2
+        mkfs.fat -F 32 -n boot /dev/${rootDevice}3
+        mount /dev/disk/by-label/nixos /mnt
+        mkdir -p /mnt/boot
+        mount /dev/disk/by-label/boot /mnt/boot
+        nixos-generate-config --root /mnt
+      ''
+    }
+  }
 }
