5e5aad7fab93328a2215cf36475bde44343d0117
Jonathan Hartman <j@hartman.io>
2024-01-17 02:02:57 -0800

    Try pulling env vars from environment.variables
    These ones don't seem to be in sessionVariables.

--- nixos/configuration-installer.nix
+++ nixos/configuration-installer.nix
@@ -23,7 +23,8 @@ in {
     after = [ "getty.target" "ncsd.service" ];
     path = [ "/run/current-system/sw" ];
     environment = config.nix.envVars // {
-      inherit (config.environment.sessionVariables) NIX_PATH NIXOS_VERSION NIXOS_HOSTNAME NIXOS_DOMAIN NIXOS_ADMIN_USER NIXOS_ADMIN_SSH_PUBLIC_KEY;
+      inherit (config.environment.sessionVariables) NIX_PATH;
+      inherit (config.environment.variables) NIXOS_VERSION NIXOS_HOSTNAME NIXOS_DOMAIN NIXOS_ADMIN_USER NIXOS_ADMIN_SSH_PUBLIC_KEY;
       HOME = "/root";
     };
     serviceConfig = {
