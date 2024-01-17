6fd3b9fdbaca41895e6a3ed89cb64b76260cbffb
Jonathan Hartman <j@hartman.io>
2024-01-17 02:10:47 -0800

    Try using `builtins.getEnv`
    I'm confused about why none of the `config.environment` options can get the
    env vars being set by GitHub. If this works, should later wrap them in error
    checking since `builtins.getEnv` returns an empty string if the var isn't set.

--- nixos/configuration-installer.nix
+++ nixos/configuration-installer.nix
@@ -24,7 +24,11 @@ in {
     path = [ "/run/current-system/sw" ];
     environment = config.nix.envVars // {
       inherit (config.environment.sessionVariables) NIX_PATH;
-      inherit (config.environment.profileRelativeSessionVariables) NIXOS_VERSION NIXOS_HOSTNAME NIXOS_DOMAIN NIXOS_ADMIN_USER NIXOS_ADMIN_SSH_PUBLIC_KEY;
+      NIXOS_VERSION = builtins.getEnv "NIXOS_VERSION";
+      NIXOS_HOSTNAME = builtins.getEnv "NIXOS_HOSTNAME";
+      NIXOS_DOMAIN = builtins.getEnv "NIXOS_DOMAIN";
+      NIXOS_ADMIN_USER = builtins.getEnv "NIXOS_ADMIN_USER";
+      NIXOS_ADMIN_SSH_PUBLIC_KEY = builtins.getEnv "NIXOS_ADMIN_SSH_PUBLIC_KEY";
       HOME = "/root";
     };
     serviceConfig = {
