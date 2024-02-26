4fd1b6dc1d6568664ab8a3d985abb101b5b9821f
Jonathan Hartman <j@hartman.io>
2024-02-26 15:34:38 -0800

    Move the DNS configs to their own file
    Them living in main was more justifiable when there was only one record.

--- tf/main.tf
+++ tf/main.tf
@@ -1,30 +1,3 @@
 terraform {
   required_version = "~> 1.6.1"
 }
-
-resource "porkbun_dns_record" "nasty_root" {
-  domain  = var.domain
-  name    = ""
-  type    = can(cidrhost("${var.ip_address}/128", 0)) ? "AAAA" : "A"
-  content = var.ip_address
-  ttl     = 600
-  notes   = "Root domain A record"
-}
-
-resource "porkbun_dns_record" "nasty_wildcard" {
-  domain  = var.domain
-  name    = "*"
-  type    = can(cidrhost("${var.ip_address}/128", 0)) ? "AAAA" : "A"
-  content = var.ip_address
-  ttl     = 600
-  notes   = "Wildcard DNS for all NAS services"
-}
-
-resource "porkbun_dns_record" "nasty_root_mx" {
-  domain  = var.domain
-  name    = ""
-  type    = "MX"
-  content = var.mx_server
-  ttl     = 600
-  notes   = "Mail server for the domain"
-}
