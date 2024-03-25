c719138408aed22765bb67562fa41d5bbdfad563
Jonathan Hartman <j@hartman.io>
2024-03-24 21:18:33 -0700

    Start sending traces from Traefik -> Grafana Agent -> Grafana Cloud
    Also changes the metrics entrypoint to only listen on localhost (just in case).

--- nixos/traefik/monitoring.nix
+++ nixos/traefik/monitoring.nix
@@ -1,6 +1,18 @@
 {config, pkgs, ...}: let
   ports = import ../variables/ports.nix;
 in {
+  services.traefik.staticConfigOptions.entryPoints.metrics = {
+    address = "localhost:${toString ports.traefik.metrics}";
+  };
+
+  services.traefik.staticConfigOptions.metrics = {
+    prometheus = {
+      entryPoint = "metrics";
+    };
+  };
+
+  services.traefik.staticConfigOptions.tracing = { };
+
   services.grafana-agent.settings.metrics.configs = [
     {
       name = "traefik";
