4330c631893a7cee2d0fbdc7206e1fde6ed4ace9
Jonathan Hartman <j@hartman.io>
2024-04-06 00:02:58 -0700

    Do an s/panel_id/uid
    I thought `panel_id` would be what I need, but that's just a double-digit integer,
    not a full alphanumeric string.

--- tf/modules/monitoring/grafana_dashboards.tf
+++ tf/modules/monitoring/grafana_dashboards.tf
@@ -24,17 +24,17 @@ resource "grafana_dashboard" "node_info" {
   config_json = templatefile(
     "${path.module}/grafana_dashboards/node_info.json.tftpl",
     {
-      pressure = grafana_library_panel.node_exporter["Pressure"].panel_id
-      cpu_busy = grafana_library_panel.node_exporter["CPU Busy"].panel_id
-      sys_load = grafana_library_panel.node_exporter["Sys Load"].panel_id
-      ram_used = grafana_library_panel.node_exporter["RAM Used"].panel_id
-      swap_used = grafana_library_panel.node_exporter["SWAP Used"].panel_id
-      root_fs_used = grafana_library_panel.node_exporter["Root FS Used"].panel_id
-      cpu_cores = grafana_library_panel.node_exporter["CPU Cores"].panel_id
-      uptime = grafana_library_panel.node_exporter["Uptime"].panel_id
-      rootfs_total = grafana_library_panel.node_exporter["RootFS Total"].panel_id
-      ram_total = grafana_library_panel.node_exporter["RAM Total"].panel_id
-      swap_total = grafana_library_panel.node_exporter["SWAP Total"].panel_id
+      pressure = grafana_library_panel.node_exporter["Pressure"].uid
+      cpu_busy = grafana_library_panel.node_exporter["CPU Busy"].uid
+      sys_load = grafana_library_panel.node_exporter["Sys Load"].uid
+      ram_used = grafana_library_panel.node_exporter["RAM Used"].uid
+      swap_used = grafana_library_panel.node_exporter["SWAP Used"].uid
+      root_fs_used = grafana_library_panel.node_exporter["Root FS Used"].uid
+      cpu_cores = grafana_library_panel.node_exporter["CPU Cores"].uid
+      uptime = grafana_library_panel.node_exporter["Uptime"].uid
+      rootfs_total = grafana_library_panel.node_exporter["RootFS Total"].uid
+      ram_total = grafana_library_panel.node_exporter["RAM Total"].uid
+      swap_total = grafana_library_panel.node_exporter["SWAP Total"].uid
     }
   )
 }
