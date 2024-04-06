8cbebac7a2a852ee8a14eaec3fcd8a27534c661e
Jonathan Hartman <j@hartman.io>
2024-04-05 23:35:18 -0700

    Start recreating the node dashboard from panels
    This may be more effort than it's worth. I could just fork the node dashboard instead...

--- tf/modules/monitoring/grafana_dashboards.tf
+++ tf/modules/monitoring/grafana_dashboards.tf
@@ -17,3 +17,25 @@ resource "grafana_library_panel" "node_exporter" {
   name       = each.key
   model_json = jsonencode(each.value)
 }
+
+resource "grafana_dashboard" "node_info" {
+  provider = grafana.stack
+
+  config_json = templatefile(
+    "${path.module}/grafana_dashboards/node_info.json.tftpl",
+    {
+      quick_cpu_mem_disk = grafana_library_panel.node_exporter["Quick CPU / Mem / Disk"].panel_id
+      pressure = grafana_library_panel.node_exporter["Pressure"].panel_id
+      cpu_busy = grafana_library_panel.node_exporter["CPU Busy"].panel_id
+      sys_load = grafana_library_panel.node_exporter["Sys Load"].panel_id
+      ram_used = grafana_library_panel.node_exporter["RAM Used"].panel_id
+      swap_used = grafana_library_panel.node_exporter["SWAP Used"].panel_id
+      root_fs_used = grafana_library_panel.node_exporter["Root FS Used"].panel_id
+      cpu_cores = grafana_library_panel.node_exporter["CPU Cores"].panel_id
+      uptime = grafana_library_panel.node_exporter["Uptime"].panel_id
+      rootfs_total = grafana_library_panel.node_exporter["RootFS Total"].panel_id
+      ram_total = grafana_library_panel.node_exporter["RAM Total"].panel_id
+      swap_total = grafana_library_panel.node_exporter["SWAP Total"].panel_id
+    }
+  )
+}
