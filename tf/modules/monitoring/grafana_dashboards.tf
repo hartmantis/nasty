b110471d6a42a96372a19fae9b2851d21699b566
Jonathan Hartman <j@hartman.io>
2024-04-15 17:01:44 -0700

    Add the ZFS pool health and disk health panels to the node dashboard

--- tf/modules/monitoring/grafana_dashboards.tf
+++ tf/modules/monitoring/grafana_dashboards.tf
@@ -42,17 +42,19 @@ resource "grafana_dashboard" "node_info" {
   config_json = templatefile(
     "${path.module}/grafana_dashboards/node_info.json.tftpl",
     {
-      pressure     = grafana_library_panel.node_exporter["Pressure"].uid
-      cpu_busy     = grafana_library_panel.node_exporter["CPU Busy"].uid
-      sys_load     = grafana_library_panel.node_exporter["Sys Load"].uid
-      ram_used     = grafana_library_panel.node_exporter["RAM Used"].uid
-      swap_used    = grafana_library_panel.node_exporter["SWAP Used"].uid
-      root_fs_used = grafana_library_panel.node_exporter["Root FS Used"].uid
-      cpu_cores    = grafana_library_panel.node_exporter["CPU Cores"].uid
-      uptime       = grafana_library_panel.node_exporter["Uptime"].uid
-      rootfs_total = grafana_library_panel.node_exporter["RootFS Total"].uid
-      ram_total    = grafana_library_panel.node_exporter["RAM Total"].uid
-      swap_total   = grafana_library_panel.node_exporter["SWAP Total"].uid
+      pressure        = grafana_library_panel.node_exporter["Pressure"].uid
+      cpu_busy        = grafana_library_panel.node_exporter["CPU Busy"].uid
+      sys_load        = grafana_library_panel.node_exporter["Sys Load"].uid
+      ram_used        = grafana_library_panel.node_exporter["RAM Used"].uid
+      swap_used       = grafana_library_panel.node_exporter["SWAP Used"].uid
+      root_fs_used    = grafana_library_panel.node_exporter["Root FS Used"].uid
+      cpu_cores       = grafana_library_panel.node_exporter["CPU Cores"].uid
+      uptime          = grafana_library_panel.node_exporter["Uptime"].uid
+      rootfs_total    = grafana_library_panel.node_exporter["RootFS Total"].uid
+      ram_total       = grafana_library_panel.node_exporter["RAM Total"].uid
+      swap_total      = grafana_library_panel.node_exporter["SWAP Total"].uid
+      zfs_pool_health = grafana_library_panel.zfs_pool_health.uid
+      unhealthy_disks = grafana_library_panel.disks_unhealthy.uid
     }
   )
 }
